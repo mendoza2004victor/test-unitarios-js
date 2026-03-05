491fa0e2ff32609329ea308ba5971a7b013f0375
Victor Eduado Mendoza Velasquez <victormendoz8@gmail.com>
2026-03-05 17:32:52 -0600

    Se agregaron más pruebas unitarias y README

--- src/funcion.js
+++ src/funcion.js
@@ -1,4 +1,8 @@
 function aplicarOperacion(a, b, operacion) {
+    if (typeof operacion !== "function") {
+        throw new Error("La operación debe ser una función");
+    }
+
     return operacion(a, b);
 }
 
--- test/funcion.test.js
+++ test/funcion.test.js
@@ -15,3 +15,17 @@ test('multiplica correctamente dos números', () => {
 
     expect(resultado).toBe(6);
 });
+
+test('resta correctamente dos números', () => {
+    const resta = (a, b) => a - b;
+
+    const resultado = aplicarOperacion(5, 3, resta);
+
+    expect(resultado).toBe(2);
+});
+
+test('lanza error si no se pasa una función', () => {
+    expect(() => {
+        aplicarOperacion(2, 3, "no es funcion");
+    }).toThrow("La operación debe ser una función");
+});
